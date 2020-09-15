91c6c9737013b311b8bdb97772d3b399985b320b
hoangnv172566 <52770888+hoangnv172566@users.noreply.github.com>
2020-09-16 01:03:28 +0700

    Tuyen Dung

--- src/main/resources/js/ajax/pages/page_chi_tiet_tuyen_dung.js
+++ src/main/resources/js/ajax/pages/page_chi_tiet_tuyen_dung.js
@@ -1,47 +1,23 @@
 $(function () {
     hiddenNavHero();
+    // let Recruitment = {...Article};
+    let articleBox = $('#article-box');
     let idRecruitment = getPageId();
-    Recruitment.mappingRecruitment(idRecruitment);
-    Recruitment.increaseView(idRecruitment).then();
+    newsFindById(idRecruitment)
+        .then(rs=>{
+            let Recruitment = {...Article};
+            Recruitment.data = rs;
+            Recruitment.endpoint = 'tuyen-dung';
+
+            let RecruitmentDetailController = {...ArticleDetailController};
+            RecruitmentDetailController.article = Recruitment;
+            RecruitmentDetailController.mappingDetailArticle(Recruitment, articleBox);
+            RecruitmentDetailController.increaseView(idRecruitment).then();
+
+        })
+        .catch(err=>console.log(err));
+
+
 });
 
-let Recruitment = {
-    getRecruitmentData: async function (idNews ) {
-        let newsData;
-        newsData = await Promise.resolve(newsFindById(idNews))
-            .then(rs => rs)
-            .catch(err => console.log(err));
-        return newsData;
-    },
 
-    generateRecuitment: function (object) {
-        $('.tinTuc__title--tagsTinTuc').html(this.handleTag(object.tag));
-        $(".tinTuc__title h2").text(object.name);
-        let templateRecuitment = $('#hidden-chiTietTinTuc').removeClass("d-none");
-        templateRecuitment.find('.tinTuc__element--img img').attr('src', viewSrcFile(object.image));
-        templateRecuitment.find('.tinTuc__title--autho').html(`được đăng
-                vào <a style="color: blue; cursor: default;" href="">${TimeUtils.formatTime(object.creatTime).d} tháng ${TimeUtils.formatTime(object.creatTime).mInNumber}</a> bởi ${object.author}`);
-        templateRecuitment.find('.tinTuc__contentTinTuc').html(object.content);
-        templateRecuitment.find('.tinTuc__previewTinTuc').text(object.preview);
-    },
-
-    handleTag: function (data) {
-        if (!(data === '' || data === null)) {
-            let listTags = data.split("|");
-            let templateTags = listTags.map(data => {
-                return `<a href="tuyen-dung?tag=${data}" class="text-uppercase" style="color: #007bff; font-size: 1.4rem;">${data}</a>`
-            }).join(', ');
-            return templateTags;
-        }
-        return '';
-    },
-
-    mappingRecruitment: function (idTinTuc) {
-        this.getRecruitmentData(idTinTuc).then(rs => this.generateRecuitment(rs)).catch(er => console.log(er));
-    },
-
-    increaseView: function (id) {
-        let uri = `news-service/api/v1/public/statistic/news/${id}/view`;
-        return ajaxPut(uri);
-    }
-};
