5c154dd48a804af2e04161da498c7138aef3bf36
ntduong10698 <44547589+ntduong10698@users.noreply.github.com>
2020-09-18 15:10:33 +0700

    san pham

--- src/main/resources/js/ajax/pages/page_san_pham.js
+++ src/main/resources/js/ajax/pages/page_san_pham.js
@@ -1,174 +1,65 @@
-var imgInforProduct, hrefImgInforProduct, promotionInforProduct, nameInforProduct, previewInforProduct , costInforProduct, statusInforProduct, propertiesInforProduct, quantityInforProduct, modelInforProduct, introductionInforProduct, catalogInforProduct, inputQuantity, btnBuyNow, listProductRelated, productTemp, unitInforProduct, inputDateUse, groupInputQuantity, listImgProduct, tempListImgProduct;
-let idPage = null;
-$(function() {
-    idPage = getPageId();
-    imgInforProduct = $(".img-product");
-    hrefImgInforProduct = $(".href-img-product");
-    promotionInforProduct = $("#pro-infor-product");
-    nameInforProduct = $("#name-infor-product");
-    costInforProduct = $("#cost-infor-product");
-    statusInforProduct = $("#status-infor-product");
-    propertiesInforProduct = $("#properties-infor-product");
-    // quantityInforProduct = $("#");
-    modelInforProduct = $("#model-infor-product");
-    introductionInforProduct = $("#introduction-infor-product");
-    catalogInforProduct = $("#file-catalog");
-    inputQuantity =$("#input-quantity");
-    btnBuyNow = $(".buynow");
-    listProductRelated = $("#list-product-related");
-    productTemp = $("#product-temp");
-    previewInforProduct = $("#preview-infor-product");
-    unitInforProduct = $("#unit-infor-product");
-    inputDateUse = $("#input-date-use");
-    groupInputQuantity = $("#group-input-quantity");
-    listImgProduct = $(".list-img-product");
-    tempListImgProduct = $(".temp-list-img-product");
-
-    getInforProduct();
+var listProductTemp, productTemp;
+$(function (){
+    listProductTemp = $("#list-product-temp");
+    productTemp = $("#product-temp")
+    viewListSanPham();
 })
 
-function getInforProduct() {
-    if(idPage) {
-        productFindById(idPage, true, true, true, true, true, false).then(rs => {
-            if(rs) {
-                statisticProductIncreseView(idPage);
-                let {id, image, name, quantity, model, introduction, preview} = rs.product;
-                let {cost} = rs.costs[0];
-                let {properties, promotion} = rs;
-                let {files} = rs;
-                let {id: idCategory, attachment} = rs.categories[0];
-                imgInforProduct.attr("src", viewSrcFile(image));
-                imgInforProduct.attr("data-zoom-image", viewSrcFile(image));
-                imgInforProduct.attr("alt", viewField(name));
-                hrefImgInforProduct.attr("href", viewSrcFile(image));
-                viewListImgProduct(files, name);
-                unitInforProduct.html(viewField(rs.costs[0].unit));
-                let {minusPrice, viewTitleGift, viewDiscount} = viewPromotionCostProduct(promotion, cost);
-                //promo
-                if(viewDiscount.length > 0) {
-                    promotionInforProduct.find(".promo-sale").html(viewDiscount);
-                } else {
-                    promotionInforProduct.find(".promo-sale").remove();
-                }
-                if(viewTitleGift.length > 0) {
-                    promotionInforProduct.find(".promo-gift").attr("title", viewTitleGift);
-                } else {
-                    promotionInforProduct.find(".promo-gift").remove();
-                }
-                runToolTip();
-                //end_promo
-                nameInforProduct.html(viewField(name));
-                previewInforProduct.html(viewField(preview));
-                //
-                if(cost != 0) {
-                    if(minusPrice == 0) {
-                        costInforProduct.find("del").remove();
-                        costInforProduct.find("span").text(viewPriceVND(cost));
-                    } else {
-                        costInforProduct.find("del").text(viewPriceVND(cost));
-                        costInforProduct.find("span").text(viewPriceVND(cost - minusPrice));
-                    }
-                    if(quantity > 0) {
-                        inputQuantity.attr("max", viewField(quantity));
-                        btnBuyNow.attr("onclick", `addToCartHasNumber(${id}, ${quantity})`);
-                    } else {
-                        groupInputQuantity.before("<strong>Sản phẩm tạm thời hết hàng vui lòng liên hệ để biết thêm thông tin! </strong>")
-                        groupInputQuantity.remove();
-                        btnBuyNow.remove();
-                    }
-                } else {
-                    costInforProduct.find("del").remove();
-                    costInforProduct.find("span").remove();
-                    groupInputQuantity.before("<strong>Liên hệ để mua sản phẩm</strong>")
-                    groupInputQuantity.remove();
-                    btnBuyNow.remove();
-                }
-                //end cost
-                statusInforProduct.html(quantity > 0 ? "Còn hàng" : "Hết hàng");
-                viewPropertiesProduct(properties, quantity);
-                modelInforProduct.html(viewField(model));
-                catalogInforProduct.attr("href", viewSrcFile(attachment));
-                introductionInforProduct.html(viewField(introduction));
-                //view list product related
-                viewProductRelated(idCategory, id);
-            }
-        }).catch(err => {
-            console.log(err);
-            alertDanger(DANGER_PRODUCT);
-        })
-    } else {
-        location.href = 404;
-    }
-}
-
-function viewPropertiesProduct(properties, quantity) {
-    let trTmp = propertiesInforProduct.find("tr");
-    let listTr = "";
-    if(properties) {
-        listTr = properties.map(data => {
-            if(data.productProperty && data.productProperty.code === "use_day") {
-                handelInputQuantityAndDateView(data.value, quantity);
-            }
-            let trClone = trTmp.clone();
-            trClone.find("th").html(viewField(data.productProperty.name));
-            trClone.find("td").html(viewField(data.value));
-            return trClone;
-        })
-    }
-    propertiesInforProduct.html(listTr);
-}
-
-function viewProductRelated(idCategory, idProduct) {
-    productFilter(COMPANY_ID, 0, idCategory, 0, "", 0, "date", true, 1, 9).then(rs => {
+async function viewListSanPham() {
+    //SAN PHAM BAN CHAY
+    await productFilter(COMPANY_ID, 0, null, 0, "", 0, "sold", false, 1, 4).then(rs => {
         if(rs) {
-            rs = rs.content.filter(data => data.id != idProduct);
-            let listProduct = getListProduct(rs);
-            listProductRelated.html(listProduct);
-            clickBtnAddCart();
-            runProductRelatedCarousel();
+            rs = rs.content;
+            let listProductClone = listProductTemp.clone().removeClass("d-none").removeAttr("id");
+            listProductClone.find(".lp-product-type-name").html("Sản phẩm bán chạy");
+            listProductClone.find(".lp-product-type-link").attr("href", "danh-muc?id=0&search=&sort=sold&asc=false");
+            let imgListProduct = listProductClone.find(".lp-product-type-img");
+            imgListProduct.attr("src", "file/icon/icon-bai-thuoc.png");
+            imgListProduct.attr("alt", "sản phẩm bán chạy");
+            listProductClone.find("#product-temp").remove();
+            listProductClone.find(".row").append(getListProduct(rs));
+            listProductTemp.before(listProductClone);
             runToolTip();
+            clickBtnAddCart();
         }
     }).catch(err => {
         console.log(err);
-        alertDanger(DANGER_PRODUCT_RELATED);
+        alertDanger(DANGER_LIST_PRODUCT_SOLD);
     })
+    //END SAN PHAM BAN CHAY
+
+    //SAN PHAM PRODUCT TYPE
+    productTypeFindByCompany(COMPANY_ID, true).then(rs => {
+        if(rs) {
+            rs.map(async data => {
+                let listProductClone = listProductTemp.clone().removeClass("d-none").removeAttr("id");
+                listProductClone.find(".lp-product-type-name").html(viewField(data.name));
+                listProductClone.find(".lp-product-type-link").attr("href", viewAliasProductType(data.alias, data.id));
+                listProductClone.attr("data-product-type", data.id);
+                let imgListProduct = listProductClone.find(".lp-product-type-img");
+                imgListProduct.attr("src", viewSrcFile(data.icon));
+                imgListProduct.attr("alt", viewField(data.name));
+                listProductClone.find("#product-temp").remove();
+                listProductTemp.before(listProductClone);
+                //call product list
+                await productFilter(COMPANY_ID, data.id, categories = null, brand = 0, text = "", status = 0, sortType = "date", asc = false, page = 1, size = 8).then(rs => {
+                    if(rs) {
+                        rs = rs.content;
+                        let listProduct = getListProduct(rs);
+                        $(`.product-wrapper[data-product-type='${data.id}'] .row`).append(listProduct);
+                        runToolTip();
+                        clickBtnAddCart();
+                    }
+                }).catch(err => {
+                    console.log(err);
+                    alertDanger(DANGER_PRODUCT_BY_PRODUCT_TYPE + data.name);
+                })
+                listProductTemp.remove();
+            })
+        }
+    }).catch(err => {
+        console.log(err);
+    })
+    //END SAN PHAM PRODUCT TYPE
 }
 
-function addToCartHasNumber(id, quantity) {
-    let val = inputQuantity.val();
-    if(regexNumber(val) && (val >= 1 || val <= quantity)) {
-        addToCart(id, val - 0);
-    }
-}
-
-function handelInputQuantityAndDateView(value, quantity) {
-    inputDateUse.attr("min", value);
-    inputDateUse.attr("max", value * quantity);
-    runInputSpinner();
-    inputQuantity.change(function () {
-        let val = inputQuantity.val();
-        inputDateUse.val(val * value);
-    })
-    inputDateUse.change(function (){
-        let val = inputDateUse.val();
-        let residual = val % value;
-        let raw = (val - residual) / value;
-        inputQuantity.val(residual > 0 ? raw + 1 : raw);
-    })
-}
-
-function viewListImgProduct(files) {
-    let view = "";
-    view = files.map(data => {
-        let cloneListImgProduct = tempListImgProduct.clone().removeClass("d-none");
-        cloneListImgProduct.attr("data-image", viewSrcFile(data.url));
-        cloneListImgProduct.attr("data-zoom-image", viewSrcFile(data.url));
-        let img = cloneListImgProduct.find("img");
-        img.attr("src", viewSrcFile(data.url));
-        img.attr("alt", viewField(data.name));
-        return cloneListImgProduct;
-    })
-    listImgProduct.html(view);
-    runListProductSlick();
-    runZoomImgProduct();
-}
